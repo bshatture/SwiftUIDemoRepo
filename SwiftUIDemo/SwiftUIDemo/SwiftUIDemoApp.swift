42bea6c6dbc7ac45e89c857f7ada015edbb293d4
Bhimashankar Hatture <hatture2010@yahoo.com>
2024-12-30 21:20:58 +0530

    initial commit after added local repository

---
+++ SwiftUIDemo/SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Bhimashankar Hatture on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
