9d818e70d2c1eb0ca2b35b3ce35f57b438b94040
Bhimashankar Hatture <hatture2010@yahoo.com>
2024-12-31 11:20:58 +0530

    Register view added

---
+++ SwiftUIDemo/SwiftUIDemo/Register/RegisterView.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterView.swift
+//  SwiftUIDemo
+//
+//  Created by Bhimashankar Hatture on 31/12/24.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .background(.red)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
