654abe6dd15f9d89a6d75bf318d14436a9e71c61
Bhimashankar <hatture2010@yahoo.com>
2026-03-11 21:58:06 +0530

    Swift UI bsh (#12)
    * Navigation path with Router added
    
    * Navigation path with Router added
    
    * Custom font added
    
    * cstome theme created
    
    * ThemeViewModel added
    
    ThemeViewModel added and injected in enviornmentObject at entry point
    
    * cuttom primaryButtonStyle added
    
    * Custom TextField Properties added
    
    * Card View with viewModifier added
    
    * Form View added ( BasicForm, FocusableFormView, DynamicToggleView, PickerView, AccountInformationView )
    
    * ValidatedFormView added ( email, password validation added with error msg )
    
    * UIDesign Constants added ( Colors, DSButton, Spacing, Typography, DesignTokens )
    
    * ProductCatalougeView, CustomModalView added
    
    * Empty CustomTransitionView added
    
    * Clean up comments in CustomTransitionView.swift
    
    Removed unnecessary comment about transition animation demo.
    
    * Clean up comments in CustomTransitionView.swift
    
    Removed comment about transition animation demo.
    
    * ActionType enum added
    
    ---------
    
    Co-authored-by: Bhimashankar Hatture <252460411+bshatturedev@users.noreply.github.com>
    Co-authored-by: Bhimashankar-Dev <hattureb@yahoo.in>

---
+++ SwiftUIDemo/SwiftUIDemo/SwiftUI_All/ViewAnimationNavigationTransition/CustomTransitionView/CustomTransitionView.swift
@@ -0,0 +1,22 @@
+//
+//  CustomTransitionView.swift
+//  SwiftUIDemo
+//
+//  Created by Bhimashankar Hatture on 11/03/26.
+// 
+
+import SwiftUI
+
+enum ActionType {
+    case next
+    case previous
+}
+
+struct CustomTransitionView: View {
+    @State private var currentStep = 1
+    
+    
+    var body: some View {
+        
+    }
+}
