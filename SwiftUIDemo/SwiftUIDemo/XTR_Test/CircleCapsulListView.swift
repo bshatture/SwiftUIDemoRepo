0304aa02036be1cfdddad1f95f6a6babe977f0e1
Bhimashankar Hatture <hattureb@yahoo.in>
2026-03-20 19:25:32 +0530

    Setting View, CircleCapsulListView added

---
+++ SwiftUIDemo/SwiftUIDemo/XTR_Test/CircleCapsulListView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleCapsulListView.swift
+//  SwiftUIDemo
+//
+//  Created by Bhimashankar Hatture on 18/03/26.
+//
+
+import SwiftUI
+
+struct CircleCapsulListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleCapsulListView()
+}
